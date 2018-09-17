7c0331bab15b902c6b6b385c8806e7ec4e2ee2dd
hieu <phuchieuct93@gmail.com>
2018-09-17 22:31:57 +0700

    Update port

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,6 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
-    port:process.env.PORT,
     proxy:{
       '/api': {
         target: "https://console.dialogflow.com/api-client/demo/embedded/616d1a7f-fb60-439c-87fb-012789a05eff",
