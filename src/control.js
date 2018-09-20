8d21cf6fbc9310b91fa86ded46c3ff6b79a42124
hieu <phuchieuct93@gmail.com>
2018-09-20 21:53:48 +0700

    Update code for control slide

--- src/control.js
+++ src/control.js
@@ -1,3 +1,42 @@
-export function startRecord(){
-    document.getElementById("mic").click()
+import $ from "jquery"
+const SLIDE_URL = "https://docs.google.com/presentation/d/1XU-_cIa7NIgExt_zEW0WEDtoPC2daiQ0FBI8Pq8fRnA/present?usp=sharing"
+
+
+function openSlide() {
+    let slideElement = $("#slide");
+    slideElement.css("display", "block")
+    slideElement.addClass('animated slower bounceInUp');
+}
+
+
+
+
+function handleSlide(result) {
+    let speech = result.result.fulfillment.speech
+    let isOpenSlide = speech.indexOf("slide") >= 0 && speech.indexOf("opening") >= 0
+    if (isOpenSlide) {
+        openSlide()
+    }
+
+
+}
+
+function playAudio(result) {
+    return new Promise((resolve, reject) => {
+        let audioElement = document.getElementById('audio-' + result.id)
+        audioElement.addEventListener("ended", function () {
+            startRecord();
+            resolve(result)
+        })
+        audioElement.play();
+
+    })
+}
+export function startRecord() {
+    $("#mic").click()
+}
+export function getResult(result) {
+    playAudio(result).then(result => {
+        handleSlide(result);
+    })
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 
-import agen from "./agent"
+import "./styles/styles.css"
+import "./styles/main.css"
+import "animate.css"
+import "./agent"
+
 import * as control from "./control"
 
 window.addEventListener('load', function(){
     control.startRecord();
 }, false )
+
+
