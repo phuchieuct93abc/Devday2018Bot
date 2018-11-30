f780854ce84011ede16a454cc4d72cd65acacab5
hieu <phuchieuct93@gmail.com>
2018-11-30 19:08:21 +0700

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -105,11 +105,10 @@ module.exports = {
         target: "https://console.dialogflow.com/api-client/demo/embedded/616d1a7f-fb60-439c-87fb-012789a05eff",
         secure: false,
         pathRewrite: {'^/api': ''}
-
-
       }
-
-    }
+    },
+    host: '0.0.0.0',
+    open: true
   },
   performance: {
     hints: false
